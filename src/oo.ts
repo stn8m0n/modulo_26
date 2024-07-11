57fc266d12abc6de2b3c7851a8f93d5bfdf71cc2
Maicon Santana <msn-maicon@hotmail.com>
2024-07-10 23:43:38 -0300

    aula6Inicio

---
+++ src/oo.ts
@@ -0,0 +1,10 @@
+//em JavaScript temos que declarar a propriedade antes
+class Pessoa {
+    nome: string;
+
+    constructor(nome: string) {
+        this.nome = nome;
+    }
+}
+
+//parei 02:19 de 15:48
