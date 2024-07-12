a3208c8fffd892530730fc62fa2c0a16855d45a6
Maicon Santana <msn-maicon@hotmail.com>
2024-07-11 23:39:24 -0300

    aula6fim

--- src/oo.ts
+++ src/oo.ts
@@ -1,10 +1,53 @@
 //em JavaScript temos que declarar a propriedade antes
 class Pessoa {
     nome: string;
+    renda?: number; //? opcional
 
-    constructor(nome: string) {
+    //o argumento opcional tem que ser posterior ao argumento opcional.
+    constructor(nome: string, renda?: number) {
         this.nome = nome;
+        this.renda = renda;
+    }
+
+    disOla(): string {
+        return `${this.nome} disse oi`;
     }
 }
 
-//parei 02:19 de 15:48
+//pode ser alterado pora da classe que herdam por ele
+//Modificadores
+class ContaBancaria{
+    protected saldo: number = 0; //private não é acessado fora da classe / protect é acessado por herança
+    public numeroConta: number;
+
+    constructor(numeroDaConta: number){
+        this.numeroConta = numeroDaConta;
+    }
+
+    static retornaNumeroDoBanco(){
+        return 125;
+    }
+    
+    private getSaldo(){
+        return this.saldo;
+    }
+
+    setSaldo(valor: number){
+        this.saldo = valor;
+    }
+//depositar
+    depositar(valor: number){
+        this.saldo += valor;
+    }
+
+}
+//extends herda de conta bancária
+class ContaBancariaPessoaFisica extends ContaBancaria {
+    depositar(valor: number): void {
+        this.saldo = valor*2;
+    }
+}
+
+const contaDoPedro = new ContaBancariaPessoaFisica (123456)
+//contaDoPedro.
+ContaBancariaPessoaFisica.retornaNumeroDoBanco()
