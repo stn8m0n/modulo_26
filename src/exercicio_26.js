1a78739dfd1384cc84052d71299e764f50b52a25
Maicon Santana <msn-maicon@hotmail.com>
2024-07-14 17:45:14 -0300

    exercicio

--- src/exercicio_26.js
+++ src/exercicio_26.js
@@ -1,14 +1,14 @@
 "use strict";
-// Função de multiplicação
+// Multiplicação
 function multiplicacao(numero1, numero2) {
     return numero1 * numero2;
 }
-// Função de saudação
+// Saudação
 function saudacao(nome) {
     return "Olá " + nome;
 }
-// Testando as funções
-const resultadoMultiplicar = multiplicacao(6, 7); // Exemplo de teste para multiplicação
-const resultadoSaudacao = saudacao("João"); // Exemplo de teste para saudação
-console.log(resultadoMultiplicar); // Deve imprimir 42
-console.log(resultadoSaudacao); // Deve imprimir "Olá João"
+// Execução funções
+const resultadoMultiplicar = multiplicacao(6, 7);
+const resultadoSaudacao = saudacao("Maicon");
+console.log(resultadoMultiplicar);
+console.log(resultadoSaudacao);
--- src/exercicio_26.ts
+++ src/exercicio_26.ts
@@ -1,16 +1,16 @@
-// Função de multiplicação
+// Multiplicação
 function multiplicacao(numero1: number, numero2: number): number {
     return numero1 * numero2;
 }
 
-// Função de saudação
+// Saudação
 function saudacao(nome: string): string {
     return "Olá " + nome;
 }
 
-// Testando as funções
-const resultadoMultiplicar = multiplicacao(6, 7); // Exemplo de teste para multiplicação
-const resultadoSaudacao = saudacao("João"); // Exemplo de teste para saudação
+// Execução funções
+const resultadoMultiplicar = multiplicacao(6, 7); 
+const resultadoSaudacao = saudacao("Maicon");
 
-console.log(resultadoMultiplicar); // Deve imprimir 42
-console.log(resultadoSaudacao); // Deve imprimir "Olá João"
+console.log(resultadoMultiplicar);
+console.log(resultadoSaudacao);
